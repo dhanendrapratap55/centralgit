b9b9cbb0ac523cec08b6006c14fc48a9b7cc1fc6
dhanendra <dhanendra@example.com>
2025-06-30 09:06:53 +0530

    2nd rg added

--- main..tf
+++ main..tf
@@ -1,4 +1,10 @@
-resouce "azuerrm_resource_group" rg1" {
-name = "rg1"
-locaion = "east us"
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1"
+  location = "east us"
+}
+
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg2"
+  location = "east us"
 }
